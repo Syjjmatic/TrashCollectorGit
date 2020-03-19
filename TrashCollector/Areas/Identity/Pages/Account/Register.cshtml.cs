db6fd02d545c5a37ed301025902ea893b074ddeb
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-19 10:03:13 -0500

    Added var roles and new SelectList to OnGetAsync method on register class

--- TrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -75,6 +75,8 @@ namespace TrashCollector.Areas.Identity.Pages.Account
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var roles = _roleManager.Roles;
+            Roles = new SelectList(roles, "Name", "Name");
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
