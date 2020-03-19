8ef5d6c4cbba248c27bb8bc8c5415f64ec96114e
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-19 10:00:13 -0500

    Added public string Role { get; set; } to Register class

--- TrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashCollector/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -64,6 +64,9 @@ namespace TrashCollector.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Role { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
